d216ac4d4ed50bf78286ed6797411d3c2a7fa74b
Alexandr Belyy <sashabelyy123@gmail.com>
2024-06-01 17:20:50 +0500

    цветное изображение

--- flask_app/app.py
+++ flask_app/app.py
@@ -9,7 +9,7 @@ base_video = 'video\\test.mp4'
 # original_vp = VideoPlayer(process_fucntion=video_process.original_with_fps,
 #                            path_to_video=base_video)
 
-original_vp = VideoPlayer(process_fucntion=video_process.gray, path_to_video=base_video)
+original_vp = VideoPlayer(process_fucntion=video_process.original, path_to_video=base_video)
 
 @app.route('/')
 def index():
