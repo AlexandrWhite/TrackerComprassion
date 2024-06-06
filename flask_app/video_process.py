6fa27845144c5b6d802290822285300445a34478
Alexandr Belyy <sashabelyy123@gmail.com>
2024-06-06 21:39:13 +0500

    Запуск с gpu работает с venv

--- flask_app/video_process.py
+++ flask_app/video_process.py
@@ -2,15 +2,13 @@ import cv2
 import torch 
 from ultralytics import YOLO 
 
-torch.cuda.set_device(0)
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = YOLO('detection_models/yolov8n.pt').to(device)
 model.fuse()
 
 def yolo(frame):
     imgsz = 320
-    results = model.predict(frame, verbose=False, classes=[2,3,5,7], imgsz=imgsz, device='gpu')
+    results = model.predict(frame, verbose=False, classes=[2,3,5,7], imgsz=imgsz,device='gpu')
     frame = results[0].plot()
     return frame
 
