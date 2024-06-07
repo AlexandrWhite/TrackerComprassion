f9a340a788d3451ad3da36271823ebfdab3bf8d0
Alexandr Belyy <sashabelyy123@gmail.com>
2024-06-07 08:15:38 +0500

    изменил gitignore

--- flask_app/video_process.py
+++ flask_app/video_process.py
@@ -3,22 +3,22 @@ import torch
 from ultralytics import YOLO 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = YOLO('detection_models/yolov8n.pt').to(device)
+model = YOLO('detection_models/yolov8x.pt').to(device)
 model.fuse()
 
 def yolo(frame):
     imgsz = 320
-    results = model.predict(frame, verbose=False, classes=[2,3,5,7], imgsz=imgsz)
+    results = model.track(frame, tracker='botsort.yaml', verbose=False, classes=[2,3,5,7], imgsz=imgsz, device='gpu')
     frame = results[0].plot()
     return frame
 
 def original(frame):
     return frame
 
-from .trackers.UCMCTrack.demo import Detection
-from .trackers.UCMCTrack.demo import Detector 
-detector = Detector()
-detector.load()
+# from trackers.UCMCTrack.demo import Detection
+# from trackers.UCMCTrack.demo import Detector 
+# detector = Detector()
+# detector.load()
 
 def ucmc_tracker(frame):
     
